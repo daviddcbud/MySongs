1955902198ddf703c0b1881c1da2894f722c43c8
daviddcbud <daviddcbud@hotmail.com>
2016-03-05 17:47:05 -0600

    first commit, getting setup

---
+++ src/SongTracker/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace SongTracker.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       
+         
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
